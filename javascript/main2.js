5f3198e6c350cad9801cc95446341b2c41e1000a
Mark Nelson <mnelson400@comcast.net>
2020-07-23 13:53:34 -0500

    Rubric Finished

--- javascript/main2.js
+++ javascript/main2.js
@@ -29,18 +29,59 @@
 
 
 // Doctor Class
+class Doctor extends Traveler {
+    constructor (name, food, isHealthy, hunt, eat) {
+        super (name, food, isHealthy, hunt, eat);
+    }
+
 // The Doctor class will extend the Traveler class and have one additional method:
 //  1. heal(traveler) - set the traveler's isHealthy property to true.
-
+    heal (traveler) {
+        if (this.isHealthy === false) {
+            return this.shouldQuarantine = false;
+        }
+        return traveler.isHealthy = true;
+    }
+}
 
 
 // Hunter Class
 // The Hunter class will extend the Traveler class, but it will start with 2 food 
-// instead of 1. The Hunter class should also have the following methods:
+// instead of 1. 
+class Hunter extends Doctor {
+    constructor (name) {
+        super (name);
+        this.food = 2;
+    }
+
+// The Hunter class should also have the following method:
 //  1. hunt() - Increase the hunter's food by 5. (A normal traveler gains only 2.)
+    hunt () {
+        this.food += 5;
+    }
+
+// The Hunter class should also have the following method:
 //  2. eat() - Consumes 2 units of food. If the hunter doesn't have 2 food when 
 //      they are instructed to eat, they eat as much as they can (0 or 1 unit), 
 //      but the hunter is no longer healthy.
+    eat () {
+        if (this.food >= 2) {
+            this.food -= 2;
+
+        } else {
+            this.food = 0;
+            this.isHealthy = false;
+        }
+    }
+
+// The Hunter class should also have the following method:
 //  3. giveFood(traveler, numOfFoodUnits) - Transfers numOfFoodUnits from the 
 //      hunter to the traveler. If the hunter doesn't have enough food, then 
-//      no food should be transferred.
+//      no food should be transfered.
+    giveFood (traveler, numOfFoodUnits) {
+        if (this.food >= numOfFoodUnits) {
+            this.food -= numOfFoodUnits;
+            traveler.food += numOfFoodUnits;
+        }
+    }
+}
--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,5 @@
+const mainElement = document.querySelector ('main');
+
     // Create a wagon that can hold 4 people
     let wagon = new Wagon(4);
     // Create five travelers
@@ -7,27 +9,77 @@
     let sarahunter = new Hunter('Sara');
     let maude = new Traveler('Maude');
     console.log(`#1: There should be 4 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+
+const divElement1 = document.createElement ('div');
+mainElement.append (divElement1);
+divElement1.append (`#1: There should be 4 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+
     wagon.join(henrietta);
     console.log(`#2: There should be 3 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+
+const divElement2 = document.createElement ('div');
+mainElement.append (divElement2);
+divElement2.append (`#2: There should be 3 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+    
     wagon.join(juan);
     wagon.join(drsmith);
     wagon.join(sarahunter);
     wagon.join(maude); // There isn't room for her!
     console.log(`#3: There should be 0 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+
+const divElement3 = document.createElement ('div');
+mainElement.append (divElement3);
+divElement3.append (`#3: There should be 0 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+
     console.log(`#4: There should be 5 total food. Actual: ${wagon.totalFood()}`);
+
+const divElement4 = document.createElement ('div');
+mainElement.append (divElement4);
+divElement4.append (`#4: There should be 5 total food. Actual: ${wagon.totalFood()}`);
+    
     sarahunter.hunt(); // gets 5 more food
     drsmith.hunt();
     console.log(`#5: There should be 12 total food. Actual: ${wagon.totalFood()}`);
+ 
+const divElement5 = document.createElement ('div');
+mainElement.append (divElement5);
+divElement5.append (`#5: There should be 12 total food. Actual: ${wagon.totalFood()}`);
+    
     henrietta.eat();
     sarahunter.eat();
     drsmith.eat();
     juan.eat();
     juan.eat(); // juan is now hungry (sick)
     console.log(`#6: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+ 
+const divElement6 = document.createElement ('div');
+mainElement.append (divElement6);
+divElement6.append (`#6: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+     
     console.log(`#7: There should be 7 total food. Actual: ${wagon.totalFood()}`);
+     
+const divElement7 = document.createElement ('div');
+mainElement.append (divElement7);
+divElement7.append (`#7: There should be 7 total food. Actual: ${wagon.totalFood()}`);
+ 
     drsmith.heal(juan);
     console.log(`#8: Quarantine should be false. Actual: ${wagon.shouldQuarantine()}`);
+     
+const divElement8 = document.createElement ('div');
+mainElement.append (divElement8);
+divElement8.append (`#8: Quarantine should be false. Actual: ${wagon.shouldQuarantine()}`);
+     
     sarahunter.giveFood(juan, 4);
     sarahunter.eat(); // She only has 1, so she eats it and is now sick
     console.log(`#9: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+     
+const divElement9 = document.createElement ('div');
+mainElement.append (divElement9);
+divElement9.append (`#9: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+         
     console.log(`#10: There should be 6 total food. Actual: ${wagon.totalFood()}`);
+     
+const divElement10 = document.createElement ('div');
+mainElement.append (divElement10);
+divElement10.append (`#10: There should be 6 total food. Actual: ${wagon.totalFood()}`);
+     
